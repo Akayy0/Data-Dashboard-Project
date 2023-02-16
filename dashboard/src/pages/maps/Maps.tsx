617b5dbf99805294f32cb9c309dde9c7416c1453
kayque <kayque.original@hotmail.com>
2023-02-16 18:27:12 -0300

    Adding flyTo function to the map

--- dashboard/src/pages/maps/Maps.tsx
+++ dashboard/src/pages/maps/Maps.tsx
@@ -1,9 +1,11 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { MapBox, MapsTitle, Wrapper } from "./Maps.style";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 //@ts-ignore
 import Geocode from "react-geocode";
 import { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { LoadingContainer, LoadingElement, LoadingText, MapBox, MapsTitle, Wrapper } from "./Maps.style";
+import { Box } from '@mui/material';
 
 Geocode.setApiKey("AIzaSyCh7lTqZXMQHjLu5xU3ij8stjfCax7fA0o");
 Geocode.setLanguage("pt-br");
@@ -11,7 +13,6 @@ Geocode.setRegion("br");
 Geocode.setLocationType("ROOFTOP");
 
 function Maps() {
-
     const params = useParams();
     const [location, setLocation] = useState<any>();
     const [loading, setLoading] = useState<boolean>(true);
@@ -20,7 +21,6 @@ function Maps() {
     useEffect(() => {
         Geocode.fromAddress(params.cityName).then(
             (response: any) => {
-
                 const { lat, lng } = response.results[0].geometry.location;
 
                 let geoLocation = {
@@ -33,28 +33,44 @@ function Maps() {
         ).finally(() => {
             setLoading(false);
         });
-    }, [])
+    }, []);
+
+    function MyComponent() {
+        const map = useMap();
+
+        useEffect(() => {
+            map.flyTo(location, map.getZoom());
+        }, [location]);
+
+        return null
+    }
 
     return (
         <Wrapper>
-
-            <MapsTitle>MAPA DE {params.cityName}</MapsTitle>
+            <MapsTitle>LOCALIZAÇÃO DE {params.cityName}</MapsTitle>
 
             {
                 loading ?
                     <>
-
+                        <MapBox>
+                            <Box style={{ minHeight: "30vw", minWidth: "100%" }}>
+                                <LoadingContainer>
+                                    <LoadingElement size={80} />
+                                    <LoadingText>Carregando...</LoadingText>
+                                </LoadingContainer>
+                            </Box>
+                        </MapBox>
                     </> :
                     (
-
                         <MapBox>
                             <MapContainer
-                                center={location}
-                                zoom={params.cityName == "Minas Gerais" ? 5 : 15}
+                                center={{ lat: -19.912998, lng: -43.940933 }}
+                                zoom={params.cityName === "Minas Gerais" ? 5 : 15}
                                 scrollWheelZoom={true}
                                 style={{ minHeight: "30vw", minWidth: "100%" }}
                                 ref={mapRef}
                             >
+                                <MyComponent />
                                 <TileLayer
                                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
